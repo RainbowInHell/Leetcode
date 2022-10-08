a9f9ef895790d30588884e4ef0d3a5ce8faa028b
RainbowInHell <matveyrfh@mail.ru>
2022-10-08 21:14:08 +0300

    Solution for Ransom Note task has been added

---
+++ Easy/Ransom_Note/Ransom_Note/Solution.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+private:
+    struct iSInSequence
+    {
+        const int d;
+        iSInSequence(int n) : d(n) {}
+        bool operator()(int n) const { return n == d; }
+    };
+
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        auto it = ransomNote.begin();
+        int cnt = ransomNote.length();
+
+        while (it != ransomNote.end())
+        {
+            if (any_of(magazine.begin(), magazine.end(), iSInSequence(*it)))
+            {
+                *find_if(magazine.begin(), magazine.end(), iSInSequence(*it)) = '~';
+                cnt--;
+                if (cnt == 0)
+                {
+                    return true;
+                }
+            }
+            it++;
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    sol.canConstruct("aa", "cbaa");
+}
